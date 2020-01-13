d052e326b9e66760acabc6d2e9132ab293c6ac15
lvdong04 <lvdong04@countrygarden.com.cn>
2020-01-13 16:44:52 +0800

    feat: support indicate packages install directory and mix mode
    1. indicate packages install directory by set prefix
    2. indicate install mode to set mode to 'iso' or 'mix'

--- syncpack-utils.bash
+++ syncpack-utils.bash
@@ -3,9 +3,69 @@
 THIS_SCRIPT_PATH=$(readlink -f "$0")
 THIS_SCRIPT_DIR=$(dirname ${THIS_SCRIPT_PATH})
 SYNCPACK_SETUP_BASH=${THIS_SCRIPT_DIR}/syncpack-setup.bash
+SYNCPACK_DEPS_PARSER=${THIS_SCRIPT_DIR}/syncpack-deps-parser.bash
 
 
 source ${SYNCPACK_SETUP_BASH}
+
+SYNC_LIST_DIR_NAME=synclists
+SYNC_DIR=${WORKSPACE_DIR}/.sync
+SYNC_LIST_DIR=${SYNC_DIR}/synclists
+PACK_TMP_PATH=${SYNC_DIR}/packages
+PACK_PATH=${WORKSPACE_DIR}
+
+
+function prepare_packages
+{
+	if [ -d ${PACK_TMP_PATH} ]
+	then
+		echo "packages temporary directory exists"
+	else
+		mkdir -p ${PACK_TMP_PATH}
+	fi
+}
+
+function init_workspace_usage
+{
+	echo "init_workspace usage:"
+	echo "   init_workspace [url] [branch] [script]: init workspace with git url"
+	echo "         - url: workspace script git url."
+	echo "         - branch: git branch of workspace sync script."
+	echo "         - script: script file name of workspace sync script."
+}
+
+
+function init_workspace
+{
+	if [ $# != 3 ]
+	then
+		echo "wrong using of init_workspace $*"
+		init_workspace_usage
+		exit -1
+	fi
+
+	init_git_url=$1
+	git_branch=$2
+	script_file=$3
+
+	mkdir -p ${SYNC_DIR}
+	cd ${SYNC_DIR}
+	echo "init with : ${init_git_url}:branch ${git_branch} file ${script_file}"
+	echo "git clone ${init_git_url} -b ${git_branch} --single-branch --depth=1 ${SYNC_LIST_DIR_NAME}"
+	git clone ${init_git_url} -b ${git_branch} --single-branch --depth=1 ${SYNC_LIST_DIR_NAME}
+	ln -sf ${SYNC_LIST_DIR_NAME}/${script_file} syncscript.bash
+}
+
+function sync_workspace
+{
+	echo "start sync workspace.."
+
+	prepare_packages
+
+	source .sync/syncscript.bash
+}
+
+
 
 function sync_file
 {
@@ -31,11 +91,56 @@ function sync_file
 	fi
 }
 
+function parse_depends_script_usage
+{
+	echo "parse_depends_script usage:"
+	echo "    parse_depends_script [script] [readonly] [mode] [prefix]"
+}
+
+function parse_depends_script
+{
+	if [ $# -ne 4 ]
+	then
+		echo "wrong using of parse_depends_script $*"
+		parse_depends_script_usage
+		exit -1
+	fi
+
+	depends_script=$1
+	pack_readonly=$2
+	pack_mode=$3
+	pack_prefix=$4
+	
+	cat ${depends_script} | xargs -I {} $0 make_rootfs {} ${pack_readonly} ${pack_mode} ${pack_prefix}
+	
+}
+
+
+function update_depends_usage
+{
+	echo "update_depends usage:"
+	echo "    update_depends [arch] [pack] [version] [readonly] [mode] [prefix]" 
+	echo "        -mode: mix or iso. In mix mode, all packages installed to same "
+	echo "               directory where prefix indicated;"
+	echo "               In iso mode, the 'prefix' directory would be seemed as base path, "
+        echo "               and each package will installed seperated at the base of base path"
+}
+
 function update_depends
 {
+	if [ $# -ne 6 ]
+	then
+		echo "wrong using of update_depends $*"
+		update_depends_usage
+		exit -1
+	fi
+
         pack_arch=$1
         pack_name=$2
         pack_version=$3
+	is_read_only=$4
+	pack_mode=$5
+	pack_prefix=$6
 
 	depends_script_full_name=${pack_name}-${pack_version}_${pack_arch}-depends.bash
 	depends_check_path=${RELEASE_SERVER_DEPENDS_ROOT}/${RELEASE_SERVER_TARGET_PLATFORM}/${pack_name}/${pack_version}
@@ -55,8 +160,8 @@ function update_depends
 		fi
 
 		echo "sync ${pack_name} depends..."
-		chmod u+x ${PACK_TMP_PATH}/${depends_script_full_name}
-		source ${PACK_TMP_PATH}/${depends_script_full_name}
+		parse_depends_script ${PACK_TMP_PATH}/${depends_script_full_name} ${is_read_only} ${pack_mode} ${pack_prefix}
+
 	else
 		#echo "${depends_script_full_name} not exist"
 		#echo "check dir ${depends_script_path}"
@@ -67,48 +172,120 @@ function update_depends
 	fi
 }
 
+function update_package_usage
+{
+	echo "update_package usage:"
+	echo "    update_package [arch] [package] [version] [prefix]"
+}
 
 function update_package
 {
-	if [ $# -ne 3 ]
+	if [ $# -ne 4 ]
 	then
-		echo "usage error update_package [arch] [pack] [version]"
+		echo "wrong using of update_package $*"
+		update_package_usage
 		exit -1;
 	fi
         pack_arch=$1
         pack_name=$2
         pack_version=$3
+	pack_prefix=$4
+
+	make_rootfs ${pack_arch} ${pack_name} ${pack_version} true iso ${pack_prefix}
+}
+
+function make_rootfs_usage
+{
+	echo "make_rootfs usage:"
+	echo "    make_rootfs [arch] [pack] [version] [readonly] [mode] [prefix]"
+	echo "        [arch]: architecher of target package"
+	echo "        [pack]: package name"
+	echo "        [version]: package version"
+	echo "        [prefix]: installation position relevated to targe rootfs"
+	echo "        [readonly]: true or false, change access permision of package to read only"
+	echo "        [mode]: iso or mix, change access permision of package to read only"
+
+	echo "make_rootfs example:"
+	echo "    make_rootfs arm64 boost 1.58.0 boost true"
+	echo ""
+	echo "    after run, the boost package would be installed to"
+	echo "    \${PWD}/package/boost, and the access permision will "
+	echo "    be changed to readonly"
+}
+
+function make_rootfs
+{
+	if [ $# -ne 6 ]
+	then
+		echo "wrong using of make_rootfs_usage $*"
+		make_rootfs_usage
+		exit -1;
+	fi
+
+        pack_arch=$1
+        pack_name=$2
+        pack_version=$3
+	is_read_only=$4
+	pack_mode=$5
+	pack_prefix=$6
         pack_full_name=${pack_name}-${pack_version}_${pack_arch}.tar.gz
+	need_update="false"
+
+
+	if [ ${pack_mode} == "iso" ]
+	then
+		install_path=${pack_prefix}/${pack_name}
+	elif [ ${pack_mode} == "mix" ]
+	then
+		install_path=${pack_prefix}
+	else
+		echo "wrong using of parse_depends_script $*"
+		parse_depends_script_usage
+		exit -2
+	fi
 
 	sync_file ${RELEASE_PATH}/${pack_name}/${pack_version}/${pack_full_name} ${PACK_TMP_PATH}
 
 	sync_file_rsl=$?
+
+	if [ -d ${PACK_PATH}/${install_path} ]
+	then 
+		chmod u+w ${PACK_PATH}/${install_path}
+		
+	fi
 	
 	if [ ${sync_file_rsl} == 1 ]
 	then
 
-		if [ -d ${PACK_PATH}/${pack_name} ]
+		if [ -d ${PACK_PATH}/${install_path} ]
 		then
-			chmod a+w ${PACK_PATH}/${pack_name}
-			rm -rf ${PACK_PATH}/${pack_name}
+			rm -rf ${PACK_PATH}/${install_path}
 		fi
 
 	elif [ ${sync_file_rsl} == 0 ]
 	then
 		echo "${pack_full_name} is up to date."
-	fi
 
-	if [ $? -lt 0 ]
-	then
+	else
 		echo "get package error occured!"
 		echo "    please check package name/version/arch is right."
 		exit -1
 	fi
 
-	if [ ! -d ${PACK_PATH}/${pack_name} ]
+	if [ ! -d ${PACK_PATH}/${install_path} ]
 	then
-		mkdir -p ${PACK_PATH}/${pack_name}
-		tar -xvzf ${PACK_TMP_PATH}/${pack_full_name} -C ${PACK_PATH}/${pack_name}
+		mkdir -p ${PACK_PATH}/${install_path}
+		need_update="true"
+	fi
+
+	if [ ${pack_mode} == "mix" ]
+	then
+		need_update="true"
+	fi
+
+	if [ "${need_update}" == "true" ]
+	then
+		tar -xvzf ${PACK_TMP_PATH}/${pack_full_name} -C ${PACK_PATH}/${install_path}
 
 		if [ $? -ne 0 ]
 		then
@@ -116,16 +293,25 @@ function update_package
 			exit -2
 		fi
 
-		chmod a-w ${PACK_PATH}/${pack_name}
-
-		if [ $? -ne 0 ]
+		if [ "${is_read_only}" == "true" ]
 		then
-			echo "change mode of package failed!"
+			chmod u-w ${PACK_PATH}/${install_path}
+			if [ $? -ne 0 ]
+			then
+				echo "change mode of package failed!"
+				exit -4
+			fi
+		elif [ "${is_read_only}" == "false" ]
+		then
+			echo "" > /dev/null
+		else
+			echo "wrong using of make_sysroot_usage $*"
+			make_sysroot_usage
 			exit -3
 		fi
 	fi
 
-	update_depends ${pack_arch} ${pack_name} ${pack_version}
+	update_depends ${pack_arch} ${pack_name} ${pack_version} ${is_read_only} ${pack_mode} ${pack_prefix}
 
 	if [ $? -ne 0 ]
 	then
--- syncpack.sh
+++ syncpack.sh
@@ -4,6 +4,7 @@ THIS_SCRIPT_PATH=$(readlink -f "$0")
 THIS_SCRIPT_DIR=$(dirname ${THIS_SCRIPT_PATH})
 SYNCPACK_SETUP_BASH=${THIS_SCRIPT_DIR}/syncpack-setup.bash
 SYNCPACK_UTILS_BASH=${THIS_SCRIPT_DIR}/syncpack-utils.bash
+WORKSPACE_DIR=${PWD}
 
 source ${SYNCPACK_UTILS_BASH}
 if [ $? -ne 0 ]
@@ -14,65 +15,11 @@ fi
 function usage
 {
 	echo "syncpack usage:"
-	echo "  syncpack COMMAND [COMMAND ARGS]"
-	echo "  example:"
-	echo "           syncpack init git@github:example/workspace"
-	echo "  COMMAND:"
-	echo "   - init [url] [branch] [script]: init workspace with git url"
-	echo "         - url: workspace script git url."
-	echo "         - branch: git branch of workspace sync script."
-	echo "         - script: script file name of workspace sync script."
-	echo "   - sync: sync workspace"
+	echo "  syncpack [COMMAND] [COMMAND ARGS]"
+	echo "  Exported Commands:"
+	echo "   - init: alias as init_workspace"
+	echo "   - sync: alias as sync_file"
 }
-
-SYNC_LIST_DIR_NAME=synclists
-WORKSPACE_DIR=${PWD}
-SYNC_DIR=${WORKSPACE_DIR}/.sync
-SYNC_LIST_DIR=${SYNC_DIR}/synclists
-PACK_TMP_PATH=${SYNC_DIR}/packages
-PACK_PATH=${WORKSPACE_DIR}/packages
-
-
-function prepare_packages
-{
-	if [ -d ${PACK_TMP_PATH} ]
-	then
-		echo "packages temporary directory exists"
-	else
-		mkdir -p ${PACK_TMP_PATH}
-	fi
-
-	if [ -d ${PACK_PATH} ]
-	then
-		echo "packages directory exists"
-	else
-		mkdir -p ${PACK_PATH}
-	fi
-}
-
-function init_workspace
-{
-	init_git_url=$1
-	git_branch=$2
-	script_file=$3
-
-	mkdir -p ${SYNC_DIR}
-	cd ${SYNC_DIR}
-	echo "init with : ${init_git_url}:branch ${git_branch} file ${script_file}"
-	echo "git clone ${init_git_url} -b ${git_branch} --single-branch --depth=1 ${SYNC_LIST_DIR_NAME}"
-	git clone ${init_git_url} -b ${git_branch} --single-branch --depth=1 ${SYNC_LIST_DIR_NAME}
-	ln -sf ${SYNC_LIST_DIR_NAME}/${script_file} syncscript.bash
-}
-
-function sync_workspace
-{
-	echo "start sync workspace.."
-
-	prepare_packages
-
-	source .sync/syncscript.bash
-}
-
 
 
 if [ $# == 0 ]
@@ -82,27 +29,25 @@ then
 fi
 
 sync_command=$1
+sync_command_args=($*)
+sync_command_args=${sync_command_args[*]:1}
+echo "sync_command_args = ${sync_command_args}"
+
 
 if test ${sync_command} = "init"
 then
-	if [ $# != 4 ]
-	then 
-		usage
-		echo "type arguments of init."
-		exit 0
-	fi
+	init_workspace ${sync_command_args}
 
-	init_workspace $2 $3 $4
-	exit 0
 elif test ${sync_command} = "sync"
 then
-	if [ $# != 1 ]
-	then
-		usage
-		exit 0
-	fi
-
 	sync_workspace
-	exit 0
+
+elif [ ${sync_command} == "make_rootfs" ]
+then
+	make_rootfs ${sync_command_args}
+else
+	echo "!!!!error!!!!! unknown command $1"
+	usage
+	exit -1
 fi
 
